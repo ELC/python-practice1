b6fd3a610fa4f0f9b81409c81689d5c0dbb997f9
lucas <lc45648siete@gmail.com>
2022-08-28 22:02:22 -0300

    Finalizado

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -9,6 +9,7 @@ y en el mismo orden.
 lista = ["casa", "perro", "pato", "gato"]
 
 # COMPLETAR - INICIO
+tupla = tuple(lista)
 
 # COMPLETAR - FIN
 
@@ -23,7 +24,7 @@ y en el mismo orden.
 tupla = "casa", "perro", "pato", "gato", "tenedor"
 
 # COMPLETAR - INICIO
-
+lista = list(tupla)
 # COMPLETAR - FIN
 
 assert lista == ["casa", "perro", "pato", "gato", "tenedor"]
@@ -36,7 +37,9 @@ Desempaquetar la siguiente tupla en las variables a, b y c
 tupla = ("primer", 25, [1, 2, 3])
 
 # COMPLETAR - INICIO
-
+a = tupla[0]
+b = tupla[1]
+c = tupla[2]
 # COMPLETAR - FIN
 
 assert a == "primer" and b == 25 and c == [1, 2, 3]
@@ -49,7 +52,8 @@ Desempaquetar la siguiente tupla y luego sumar sus valores
 tupla = (87, 98, 35, 67, 4, 9)
 
 # COMPLETAR - INICIO
-
+a,b,c,d,e,f = tupla
+total = a + b + c + d + e + f
 # COMPLETAR - FIN
 
 assert total == 300
@@ -63,7 +67,9 @@ Restricción: Utilizar f-Strings.
 lista = ["esta", "mañana", "sali", "a", "correr"]
 
 # COMPLETAR - INICIO
+a,b,c,d,e = lista
 
+string_concatenado = f'{a} {b} {c} {d} {e}'
 # COMPLETAR - FIN
 
 assert string_concatenado == "esta mañana sali a correr"
@@ -77,7 +83,7 @@ Restricción: Utilizar desempaquetado con comodines
 tupla = (73, 45, 344, 3434, 2)
 
 # COMPLETAR - INICIO
-
+primer, *rest = tupla
 # COMPLETAR - FIN
 
 assert primer == 73
@@ -91,7 +97,9 @@ Restricción: Utilizar desempaquetado con comodines
 lista = [73, 45, 344, 3434, 2]
 
 # COMPLETAR - INICIO
+primer, *rest, ultimo = lista
 
+suma= primer + ultimo
 # COMPLETAR - FIN
 
 assert suma == 75
@@ -106,7 +114,9 @@ Restricción: Utilizar desempaquetado con comodines y f-Strings
 tupla = ("anoche", "fui", "a", "la", "fiesta", "pero", "no", "pude", "entrar")
 
 # COMPLETAR - INICIO
+a,b,c,d,e, *rest = tupla
 
+string_concatenado = f'{a} {b} {c} {d} {e}'
 # COMPLETAR - FIN
 
 assert string_concatenado == "anoche fui a la fiesta"
--- exercises/exercise8.py
+++ exercises/exercise8.py
@@ -10,6 +10,12 @@ Definir un diccionario para un 'Cliente' que contenga los siguiente valores:
 """
 
 # COMPLETAR - INICIO
+Cliente = {
+    "Nombre":"Mario Pedernera",
+    "DNI":56895632,
+    "Domicilio":"Los alamos 4509",
+    "Compras":["cafetera", "TV 50 pulgadas", "mouse gamer"],
+}
 
 # COMPLETAR - FIN
 
@@ -29,11 +35,15 @@ Definir un diccionario para las 'Compras' que contenga los siguiente valores:
 """
 
 # COMPLETAR - INICIO
-
+Compras = {
+    "Mario Pedernera":["cafetera", "TV 50 pulgadas", "mouse gamer"],
+    "Ezequiel Castello":["ipad", "ipod", "iphone"],
+    "Pablo Piristrelli":["Reproductor de CD", "Videograbadora"],
+}
 # COMPLETAR - FIN
 
 assert (
-    (Compras["Mario Pedernera"] == ["cafetera", "TV 50 pulgads", "mouse gamer"])
+    (Compras["Mario Pedernera"] == ["cafetera", "TV 50 pulgadas", "mouse gamer"])
     and (Compras["Ezequiel Castello"] == ["ipad", "ipod", "iphone"])
     and (Compras["Pablo Piristrelli"] == ["Reproductor de CD", "Videograbadora"])
 )
@@ -52,7 +62,7 @@ diccionario = {
 }
 
 # COMPLETAR - INICIO
-
+clave1 = diccionario.get("clave1")
 # COMPLETAR - FIN
 
 assert clave1 == 234
@@ -72,6 +82,7 @@ diccionario_2 = {
 }
 
 # COMPLETAR - INICIO
+clave5 = diccionario_2.get("clave5", 5)
 
 # COMPLETAR - FIN
 
@@ -90,7 +101,8 @@ diccionario_3 = {
 }
 
 # COMPLETAR - INICIO
-
+keys = []
+keys = list(diccionario_3.keys())
 # COMPLETAR - FIN
 
 assert keys == ["clave1", "clave2", "clave3", "clave4"]
@@ -108,7 +120,8 @@ diccionario_4 = {
 }
 
 # COMPLETAR - INICIO
-
+values = []
+values = list(diccionario_4.values())
 # COMPLETAR - FIN
 
 assert values == [1234, 4567, 8910, 1112]
@@ -127,7 +140,8 @@ diccionario_5 = {
 }
 
 # COMPLETAR - INICIO
-
+items = []
+items = list(diccionario_5.items())
 # COMPLETAR - FIN
 
 assert items == [(1, 1111), (2, 2222), (3, 3333), (4, 4444)]
@@ -152,7 +166,7 @@ diccionario_7 = {
 }
 
 # COMPLETAR - INICIO
-
+diccionario_6.update(diccionario_7)
 # COMPLETAR - FIN
 
 assert diccionario_6 == {1: 1111, 2: 2223, 3: 3334, 4: 4444, 5: 5555, 6: 6666}
